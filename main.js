76b6a466639111e3e039a56090a7caeb3bb95132
matthew <matthewallen235@gmail.com>
2024-09-02 22:46:01 +0100

    java

---
+++ main.js
@@ -0,0 +1,34 @@
+//console.log("Hello world");
+//console.log(5 + 5);
+
+//alert("what is your name:");
+
+//var visitorname = prompt("whats your name;");
+//alert("my name is " + visitorname);
+//console.log(visitorname);
+
+console.log(20 > 100);
+
+("mikel");
+
+var mikel = 20;
+var matthew = 100;
+
+console.log(mikel + matthew);
+
+console.log(5 <= 5);
+console.log(5 < 5);
+
+//Diclaring different variables of different data types
+let firstName = "Asabeneh";
+let lastName = "Nnamani";
+let country = "Nigeria";
+let city = "finland";
+let job = "teacher";
+let age = 250;
+let space = " ";
+let fullName = firstName + space + lastName;
+let personInfoOne = fullName + ".I am" + age + ".I Live in" + country;
+console.log(personInfoOne);
+
+let personInfoTwo = `I am ${fullName}.I am ${age}. I live in ${country} .`;
